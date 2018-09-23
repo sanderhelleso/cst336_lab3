ebcabf35078c2df8046eb4bde68dfc099db41eea
Zackery Hutchinson <zhutchinson@csumb.edu>
2018-09-23 04:00:22 +0000

    some css styles for the page

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
-<?php
-    include './php/main.php';
-?>
 
 <!DOCTYPE html>
 <html>
-    
+    <head>
+         <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <header>
+        <div id="title">
+            <h1>Silver Jack Card Game!!</h1>
+        </div>
+    </header>
+    <body id="background">
+        <div id="border">
+            <div id="cardtable">
+            <?php
+                include './php/main.php';
+            ?>
+            </div>
+        </div>
+    </body>
 </html>
